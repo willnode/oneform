76095fc42276fce3b1a250f76d0e486d9352e7f4
Julius <juliusagung123@gmail.com>
2024-07-06 22:38:17 +0700

    feat: base material tailwind setup & adjust style

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -31,6 +31,15 @@ export default withMT({
     "./node_modules/@material-tailwind/html/**",
   ],
   theme: {
+    container: {
+      screens: {
+        sm: '100%',
+        md: '100%',
+        lg: '100%',
+        xl: '100%',
+        '2xl': '100%',
+      },
+    },
     fontFamily: {
       fontFamily,
     },
