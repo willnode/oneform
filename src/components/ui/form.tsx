0178426fbc26eed34c40499f0b44b4f8ef9ce4bb
Wildan M <willnode@wellosoft.net>
2024-07-20 22:29:07 +0700

    Add all shacdn ui

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -3,9 +3,9 @@ import * as LabelPrimitive from "@radix-ui/react-label"
 import { Slot } from "@radix-ui/react-slot"
 import {
   Controller,
-  type ControllerProps,
-  type FieldPath,
-  type FieldValues,
+  ControllerProps,
+  FieldPath,
+  FieldValues,
   FormProvider,
   useFormContext,
 } from "react-hook-form"
