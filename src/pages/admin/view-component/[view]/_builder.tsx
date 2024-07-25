c6063add2851d7a511c1913afc6c4f8e43344d7c
Wildan M <willnode@wellosoft.net>
2024-07-25 20:28:53 +0700

    Add indentation support

--- src/pages/admin/view-component/[view]/_builder.tsx
+++ src/pages/admin/view-component/[view]/_builder.tsx
@@ -33,6 +33,70 @@ type Props = {
   config: string | null,
 }
 
+const indent = 2;
+
+// https://stackoverflow.com/a/46063843/3908409
+const keyDownModifier: React.KeyboardEventHandler<HTMLTextAreaElement> = (e) => {
+  const c = e.currentTarget;
+  if (e.which == 13) // ASCII newline
+  {
+    requestAnimationFrame(function () {
+      var start = c.selectionStart;
+      var v = c.value;
+      var thisLine = "";
+      var indentation = 0;
+      for (var i = start - 2; i >= 0 && v[i] != "\n"; i--) {
+        thisLine = v[i] + thisLine;
+      }
+      for (var i = 0; i < thisLine.length && thisLine[i] == " "; i++) {
+
+        indentation++;
+      }
+      c.value = v.slice(0, start) + " ".repeat(indentation) + v.slice(start);
+      c.selectionStart = start + indentation;
+      c.selectionEnd = start + indentation;
+    });
+  }
+  if (e.which == 9) //ASCII tab
+  {
+    e.preventDefault();
+    var start = c.selectionStart;
+    var end = c.selectionEnd;
+    var v = c.value;
+    if (start == end) {
+      c.value = v.slice(0, start) + " ".repeat(indent) + v.slice(start);
+      c.selectionStart = start + indent;
+      c.selectionEnd = start + indent;
+      return;
+    }
+
+    var selectedLines = [];
+    var inSelection = false;
+    var lineNumber = 0;
+    for (var i = 0; i < v.length; i++) {
+      if (i == start) {
+        inSelection = true;
+        selectedLines.push(lineNumber);
+      }
+      if (i >= end)
+        inSelection = false;
+
+      if (v[i] == "\n") {
+        lineNumber++;
+        if (inSelection)
+          selectedLines.push(lineNumber);
+      }
+    }
+    var lines = v.split("\n");
+    for (var i = 0; i < selectedLines.length; i++) {
+      lines[selectedLines[i]] = " ".repeat(indent) + lines[selectedLines[i]];
+    }
+
+    c.value = lines.join("\n");
+  }
+
+}
+
 // Render Puck editor
 export default function Editor({ id, schema, config }: Props) {
   const form = useForm({
@@ -55,12 +119,13 @@ export default function Editor({ id, schema, config }: Props) {
           render={({ field }) => (
             <FormItem className="grow">
               <Textarea
-                className="form-control"
+                className="font-mono"
                 defaultValue={field.value || ''}
                 name="config"
                 onChange={field.onChange}
                 placeholder={"# Config\nmock:\n  title: My Component!"}
                 rows={16}
+                onKeyDown={keyDownModifier}
               />
             </FormItem>
           )} />
@@ -70,12 +135,13 @@ export default function Editor({ id, schema, config }: Props) {
           render={({ field }) => (
             <FormItem className="grow">
               <Textarea
-                className="form-control"
+                className="font-mono"
                 defaultValue={field.value || ''}
                 name="schema"
                 onChange={field.onChange}
                 placeholder={"<!-- HTML body\n(style: use @import instead of href)\n(scripts: use this instead of document) -->\n<div id='tag'></div>\n<script src='https://unpkg.com/jquery/dist/jquery.min.js'>\n<script>$('#tag').text(this.dataset.title)</script>"}
                 rows={16}
+                onKeyDown={keyDownModifier}
               />
             </FormItem>
           )} />
