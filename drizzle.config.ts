78f1b2f2c70f394b3a8a1c33d85d2d42ea498dc5
Wildan M <willnode@wellosoft.net>
2024-07-26 20:14:25 +0700

    Fix envar

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   dbCredentials: {
     user: process.env.DB_USERNAME || 'root',
     password: process.env.DB_PASSWORD || '',
-    host: process.env.DB_HOST || '127.0.0.1' || 'localhost',
+    host: process.env.DB_HOST || '127.0.0.1',
     port: parseInt(process.env.DB_PORT || '3306'),
     database: process.env.DB_NAME || 'oneform_db',
   },
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,12 +3,14 @@ import {migrate} from 'drizzle-orm/mysql2/migrator';
 
 import * as schema from '@/db/schema';
 import mysql from 'mysql2/promise';
+
+// import.meta.env saves envar at build time
 const mySqlConfig = {
-  host: process.env.DB_HOST || '127.0.0.1' || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'oneform_db',
-  port: parseInt(process.env.DB_PORT || '3306'),
+  host: import.meta.env.DB_HOST || '127.0.0.1',
+  user: import.meta.env.DB_USER || 'root',
+  password: import.meta.env.DB_PASSWORD || '',
+  database: import.meta.env.DB_NAME || 'oneform_db',
+  port: parseInt(import.meta.env.DB_PORT || '3306'),
 };
 
 const sql = mysql.createPool(mySqlConfig);
